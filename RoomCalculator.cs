cbfbc5dbee0bd18f6305b7b1dd5185f753d88e2c
Savanah Conrad <sibgym@yahoo.com>
2019-07-14 21:55:07 -0400

    First commit

---
+++ RoomCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Room_Calculator
+{
+    class RoomCalculator
+    {
+        private readonly double length;
+        private readonly double width;
+
+        public RoomCalculator()
+        {
+            length = 0;
+            width = 0;
+        }
+
+        public RoomCalculator(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double Area()
+        {
+            return length * width;
+        }
+
+        public double Perimeter()
+        {
+            return (length * 2) + (width * 2);
+        }
+
+        public double Volume(double height)
+        {
+            return length * width * height;
+
+        }
+       
+        public override string ToString()
+        {
+            return $"Area: {Area()} \nPerimeter: {Perimeter()}";
+        }
+    }
+}
---
+++ RoomCalculatorTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Room_Calculator
+{
+    public class RoomCalculatorTest
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Room Detail Generator!");
+
+            char userDecision;
+            char userWouldLikeVolume;
+            do
+            {
+                Console.WriteLine("\nEnter Width:");
+                double width = Convert.ToDouble(Console.ReadLine()); 
+                Console.WriteLine("Enter Length:");
+                double length = Convert.ToDouble(Console.ReadLine()); 
+                RoomCalculator room1 = new RoomCalculator(length, width);
+                Console.WriteLine(room1);
+                Console.WriteLine("\nWould you like to calculate the volume of the room? Press 'Y' for yes and 'N' for no.");
+                userWouldLikeVolume = Console.ReadKey().KeyChar;
+                if (userWouldLikeVolume == 'Y' || userWouldLikeVolume == 'y')
+                {
+                    Console.WriteLine("\nPlease enter the room's height:");
+                    double height = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Volume:" + room1.Volume(height));
+                }
+                Console.WriteLine("\nWould you like to calculate for another room? Press 'Y' for yes and 'N' for no.");
+                userDecision = Console.ReadKey().KeyChar;
+            } while (userDecision == 'Y' || userDecision == 'y'); 
+
+        }
+    }
+}
+
+
+
+
+
