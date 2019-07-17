b9b544cdbb9f55d0aa9d3e2f69ad7bddd1410aff
Savanah Conrad <sibgym@yahoo.com>
2019-07-17 13:48:42 -0400

    Updates to formatting and comments

--- RoomCalculator.cs
+++ RoomCalculator.cs
@@ -13,29 +13,29 @@ namespace Room_Calculator
             width = 0;
         }
 
-        public RoomCalculator(double length, double width)
+        public RoomCalculator(double length, double width) 
         {
             this.length = length;
             this.width = width;
         }
 
-        public double Area()
+        public double Area() // Calculates area 
         {
             return length * width;
         }
 
-        public double Perimeter()
+        public double Perimeter() // Calculates perimeter
         {
             return (length * 2) + (width * 2);
         }
 
-        public double Volume(double height)
+        public double Volume(double height) // Calculates volume
         {
             return length * width * height;
 
         }
        
-        public override string ToString()
+        public override string ToString() // ToString method to format the area and perimeter 
         {
             return $"Area: {Area()} \nPerimeter: {Perimeter()}";
         }
--- RoomCalculatorTest.cs
+++ RoomCalculatorTest.cs
@@ -9,26 +9,36 @@ namespace Room_Calculator
 
             char userDecision;
             char userWouldLikeVolume;
+
             do
             {
                 Console.WriteLine("\nEnter Width:");
                 double width = Convert.ToDouble(Console.ReadLine()); 
+
                 Console.WriteLine("Enter Length:");
-                double length = Convert.ToDouble(Console.ReadLine()); 
-                RoomCalculator room1 = new RoomCalculator(length, width);
+                double length = Convert.ToDouble(Console.ReadLine());
+                 
+                // Creates new instance of the RoomCalculator class which takes in the user's length and width
+                RoomCalculator room1 = new RoomCalculator(length, width); 
                 Console.WriteLine(room1);
+
                 Console.Write("\nWould you like to calculate the volume of the room? Press 'Y' for yes and 'N' for no.");
-                // userWouldLikeVolume = Console.ReadKey().KeyChar;
                 userWouldLikeVolume = Console.ReadKey().KeyChar;
+
+                // Asks the user if they want to calaculate volume - if they do it executes and if they dont the program continues
                 if (userWouldLikeVolume == 'Y' || userWouldLikeVolume == 'y')
                 {
                     Console.WriteLine("\nPlease enter the room's height:");
                     double height = Convert.ToDouble(Console.ReadLine());
                     Console.WriteLine("Volume:" + room1.Volume(height));
                 }
+
+                // Asks the user if they want to continue or end the program
                 Console.Write("\nWould you like to calculate for another room? Press 'Y' for yes and 'N' for no.\n");
                 userDecision = Console.ReadKey().KeyChar;
+
             } while (userDecision == 'Y' || userDecision == 'y');
+
             Console.WriteLine("\n\nThank you, and goodbye. ");
         }
     }
